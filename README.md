7e612bde93e91936a8d95b844651cddd85780941
François Petitit <fpetitit@octo.com>
2015-10-02 16:00:49 +0200

    ajout test page d'accueil

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ka-ching-phantomcss
+module contenant des tests PhantomCSS pour Ka-ching
+
+Pour installer et configuer PhantomCSS :
+https://github.com/Huddle/PhantomCSS
+
+Pour lancer les tests :
+casperjs test ./test/testsuite.js
---
+++ test/testsuite.js
@@ -0,0 +1,41 @@
+var phantomcss = require('./../phantomcss.js');
+
+phantomcss.init({
+    mismatchTolerance: 1
+});
+
+var welcomePageURL = 'http://localhost:3000';
+
+casper.start('/');
+
+casper.viewport(1024, 1024);
+
+casper.thenOpen(welcomePageURL, function () {
+    phantomcss.screenshot('body', 'whole login page');
+});
+
+casper.then(function fill_in() {
+    this.fill('form[name="loginForm"]', {
+        'login': 'john.doe',
+        'password': 'foobar'
+    });
+    this.click('button[type="submit"]');
+    phantomcss.screenshot('body', 'whole home page');
+});
+
+casper.then(function now_check_the_screenshots() {
+    phantomcss.compareAll();
+});
+
+casper.then(function end_it() {
+    casper.test.done();
+});
+
+/*
+ Casper runs tests
+ */
+casper.run(function () {
+    console.log('\nTHE END.');
+    phantom.exit(phantomcss.getExitStatus());
+});
+
